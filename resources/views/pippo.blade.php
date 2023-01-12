850c4dfb5cd5a78b51594bd32f1d18f7bcf460fc
aCute5 <costantini95dario@gmail.com>
2023-01-12 17:08:11 +0100

    adding pippo page

--- resources/views/pippo.blade.php
+++ resources/views/pippo.blade.php
@@ -81,11 +81,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello World
+                Pippo
                 </div>
                 <div class="links">
-                    <a href="http://127.0.0.1:8000/pippo">Pippo</a>
-
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/pippo', function () {
+    return view('pippo');
+});
